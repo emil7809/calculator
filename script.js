bf7e8d36de0aa9baf9419ab905980ce9017a3676
emily <emil7809@gmail.com>
2021-08-29 14:43:24 +0200

    Activity Digram implanted - thats a lot of functions yo

---
+++ script.js
@@ -0,0 +1,59 @@
+"use strict";
+
+window.addEventListener("load", settingUp);
+
+function settingUp(){
+    console.log("Setting up");
+};
+
+function readFirstNumber(){
+    console.log("readFirstNumber");
+};
+
+function readSecondNumber(){
+    console.log("readSecondNumber");
+};
+
+function readOperator(){
+    console.log("readOperator");
+};
+
+function calculateFirstPlusSecond(){
+    console.log("calculateFirstSecond");
+};
+
+function calculateFirstMinusSecond(){
+    console.log("calculateFirstSecond");
+};
+
+function calculateFirstDevideSecond(){
+    console.log("calculateFirstSecond");
+};
+
+function calculateFirstTimesSecond(){
+    console.log("calculateFirstSecond");
+};
+
+function checkIfResultShouldBeRounded(){
+    console.log("checkIfResultShouldBeRounded");
+};
+
+function readNumberOfDecimals(){
+    console.log("readNumberOfDecimals");
+};
+
+function roundNumberogDecimals(){
+    console.log("roundNumberogDecimals");
+};
+
+function writeResultIntoFirstNumberField(){
+    console.log("writeResultIntoFirstNumberField");
+};
+
+function appendResultToEndOfList(){
+    console.log("appendResultToEndOfList");
+};
+
+function ScrollListToTheBottom(){
+    console.log("ScrollListToTheBottom");
+};
